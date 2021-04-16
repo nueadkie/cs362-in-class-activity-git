37de765225fd765592b87f7fa5b7093cc1dee67a
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-04-15 17:28:07 -0700

    added list of symbols from which a password could be generated

--- password.py
+++ password.py
@@ -1,6 +1,8 @@
 import random
 
 def password_gen(n):
-  password = []
+  symbols = "!@#$%^&*()QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm"
+  password = ""
   for i in range(n):
     pass
+  return password
