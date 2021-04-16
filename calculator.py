f4c1e66fcc17515f83f78d5aa1cfabd162abc201
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-04-15 17:01:12 -0700

    list and sum of list

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 def calc(a, b):
-  sum = a + b
+  calc_sum = a + b
   diff = a - b
   multiple = a * b
   quotient = a / b
+  num_list = [calc_sum, diff, multiple, quotient]
+  sum_list = sum(num_list)
